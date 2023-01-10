d3bb4668d3e59963a9291cf879f7ac55f6bd010c
Krystian Piątkowski <krystianpiatkowski@gmail.com>
2023-01-10 11:53:24 +0100

    Added plan data fetch key names conversion

--- src/contexts/plan.js
+++ src/contexts/plan.js
@@ -6,7 +6,7 @@ const PlanContext = createContext();
 
 export const PlanContextProvider = ({ children }) => {
   const [plan, setPlan] = useState([]);
-  const {token} = useContext(AuthContext);
+  const { token } = useContext(AuthContext);
 
   const addWeek = async (week) => {
     const { id } = JSON.parse(localStorage.getItem("user"));
@@ -23,10 +23,77 @@ export const PlanContextProvider = ({ children }) => {
     }
   };
 
+  const testData = {
+    days: {
+      monday: {
+        meals: {
+          recipes_id: [99, 66],
+          ingredients_id: [544, 545],
+        },
+      },
+      tuesday: {
+        meals: {
+          recipes_id: [],
+          ingredients_id: [],
+        },
+      },
+      wednesday: {
+        meals: {
+          recipes_id: [],
+          ingredients_id: [],
+        },
+      },
+      thursday: {
+        meals: {
+          recipes_id: [],
+          ingredients_id: [],
+        },
+      },
+      friday: {
+        meals: {
+          recipes_id: [],
+          ingredients_id: [],
+        },
+      },
+      saturday: {
+        meals: {
+          recipes_id: [],
+          ingredients_id: [],
+        },
+      },
+      sunday: {
+        meals: {
+          recipes_id: [],
+          ingredients_id: [],
+        },
+      },
+    },
+    users_id: 21,
+    sync: "false",
+    start_date: "2022-01-10",
+    end_date: "2022-01-16",
+  };
+
   useEffect(() => {
     async function fetchData() {
       console.log("fetching plan..."); //*: dev only line
       const response = await fetchPlan(token);
+
+      // Change response key names for ingtegrity (recipes_id and ingredients_id are converted but names persist)
+      // ? Not optimal but i don't know how to change object key names inside xano.com after conversion from just an id
+      response.data.forEach((week) => {
+        for (const [key, value] of Object.entries(week.days)) {
+          const ingredients = value.meals.ingredients_id;
+          const recipes = value.meals.recipes_id;
+
+          delete Object.assign(week.days[key].meals, { recipes: recipes })[
+            "recipes_id"
+          ];
+          delete Object.assign(week.days[key].meals, {
+            ingredients: ingredients,
+          })["ingredients_id"];
+        }
+      });
 
       setPlan(response.data);
     }
