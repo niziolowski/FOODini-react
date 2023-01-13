ba326bf42e2a67867ba77913e48a49d1ad6d84bd
Krystian <krystianpiatkowski92@gmail.com>
2023-01-13 14:07:37 +0100

    Added controls along with subtitle display to mobile nav

--- src/components/NavMobile/NavMobile.js
+++ src/components/NavMobile/NavMobile.js
@@ -8,16 +8,18 @@ import {
   FiBookOpen,
   FiShoppingBag,
 } from "react-icons/fi";
-import React, { Fragment, useContext, useState } from "react";
+import React, { Fragment, useContext, useState, useMemo } from "react";
 import ReactDOM from "react-dom";
 import NavMobileBar from "./NavMobileBar";
 import Button from "../UI/Button/Button";
 import LayoutContext from "../../contexts/layout";
+import PlanContext from "../../contexts/plan";
 
 function NavMobile() {
   const [activeTab, setActiveTab] = useState("home");
-
   const { dispatchIsVisible } = useContext(LayoutContext);
+  const { activeWeek, currentWeek, previousWeek, nextWeek, setActiveWeek } =
+    useContext(PlanContext);
 
   // Set of tabs and respective icons
   const tabs = [
@@ -44,12 +46,42 @@ function NavMobile() {
   ];
 
   function handleTabClick(tab) {
+    // If home tab is active and tapped, set active week to current week
+    if (tab === activeTab) setActiveWeek(currentWeek);
+
     // Set tab as active
     setActiveTab(tab);
 
     // Show clicked tab
     dispatchIsVisible({ type: tab, mode: "switch" });
   }
+
+  const handleControlsClick = (e) => {
+    const btn = e.target.closest("button");
+
+    // Previous week
+    if (btn.classList.contains("js-previous-week")) previousWeek();
+
+    // Next week
+    if (btn.classList.contains("js-next-week")) nextWeek();
+  };
+
+  // Format the subtitle when user changes active week
+  const subtitleText = useMemo(() => {
+    if (!activeWeek) return;
+    const startDate = new Date(activeWeek.start_date).toLocaleDateString(
+      "pl-PL",
+      {
+        month: "long",
+        day: "numeric",
+      }
+    );
+    const endDate = new Date(activeWeek.end_date).toLocaleDateString("pl-PL", {
+      month: "long",
+      day: "numeric",
+    });
+    return `${startDate} - ${endDate}`;
+  }, [activeWeek]);
 
   const navBarEl = (
     <div className={styles.navbar}>
@@ -68,14 +100,24 @@ function NavMobile() {
     <Fragment>
       <div className={styles.nav}>
         <div className={styles["top-nav"]}>
-          <Button round mini>
+          <Button
+            className="js-previous-week"
+            onClick={handleControlsClick}
+            round
+            mini
+          >
             <FiChevronLeft />
           </Button>
           <div className={styles.title}>
             <h1>PLAN POSIŁKÓW</h1>
-            <h2>BIEŻĄCY TYDZIEŃ</h2>
+            <h2>{subtitleText}</h2>
           </div>
-          <Button round mini>
+          <Button
+            className="js-next-week"
+            onClick={handleControlsClick}
+            round
+            mini
+          >
             <FiChevronRight />
           </Button>
         </div>
