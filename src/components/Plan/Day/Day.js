c85389556a6eb711362d6935d0aabf046f0e8ff5
Krystian <krystianpiatkowski92@gmail.com>
2023-01-16 07:33:34 +0100

    Added NewMeal handlers to Day component

--- src/components/Plan/Day/Day.js
+++ src/components/Plan/Day/Day.js
@@ -1,29 +1,37 @@
 import styles from "./Day.module.css";
 import Button from "../../UI/Button/Button";
 import { FiPlus } from "react-icons/fi";
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import LayoutContext from "../../../contexts/layout";
 import Meal from "./Meal/Meal";
 
-function Day({ title, meals }) {
+function Day({ title, meals, onNewMeal }) {
   const { isMobile } = useContext(LayoutContext);
 
+  const handleClick = (e) => {
+    const btn = e.target.closest("button");
+
+    // Add meal
+    if (btn.classList.contains("js-add-meal")) onNewMeal();
+  };
+
   const btnAdd = (
-    <Button primary round mini>
+    <Button className="js-add-meal" onClick={handleClick} primary round mini>
       <FiPlus />
     </Button>
   );
 
   const classes = `${styles.day} ${isMobile && styles.mobile}`;
 
+  const mealsContent = useMemo(() => {
+    if (!meals) return null;
+    meals.map((meal) => <Meal key={meal.app_id} title={meal.name} />);
+  }, [meals]);
+
   return (
     <div className={classes}>
       <div className={styles.title}>{title}</div>
-      <ul className={styles.list}>
-        {meals.map((meal) => (
-          <Meal key={meal.app_id} title={meal.name} />
-        ))}
-      </ul>
+      <ul className={styles.list}>{meals && mealsContent}</ul>
       {btnAdd}
     </div>
   );
