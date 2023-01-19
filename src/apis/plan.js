def130e944a34f6ebe3d5883b79430cd96997a4a
Krystian <krystianpiatkowski92@gmail.com>
2023-01-19 10:31:28 +0100

    Added a new API call to /apis/plan

--- src/apis/plan.js
+++ src/apis/plan.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 //! Move to env
-const baseURL = "https://x8ki-letl-twmt.n7.xano.io/api:P_BSkInF/plan";
+const BASE_URL = "https://x8ki-letl-twmt.n7.xano.io/api:P_BSkInF/plan";
+const CUSTOMS_URL =
+  "https://x8ki-letl-twmt.n7.xano.io/api:as7xy9qf/editMultipleWeeks";
 
 // Create axios instance with authorization header
 const authorize = (token) => {
@@ -12,14 +14,23 @@ const authorize = (token) => {
   };
 };
 
+// BASIC CRUD
+
 // Get the plan
-export const fetchPlan = (token) => axios.get(baseURL, authorize(token));
+export const fetchPlan = (token) => axios.get(BASE_URL, authorize(token));
 
 // Create Week
-export const createWeek = (week) => axios.post(baseURL, week);
+export const createWeek = (week) => axios.post(BASE_URL, week);
 
 // Update Week
-export const updateWeek = (week) => axios.post(`${baseURL}/${week.id}`, week);
+export const updateWeek = (week) => axios.post(`${BASE_URL}/${week.id}`, week);
 
 //   Delete week
-export const deleteWeek = (id) => axios.delete(`${baseURL}/${id}`);
+export const deleteWeek = (id) => axios.delete(`${BASE_URL}/${id}`);
+
+// CUSTOM CALLS
+
+// Update multiple weeks
+// payload = array of weeks to update
+export const updateMultipleWeeks = (payload, token) =>
+  axios.post(CUSTOMS_URL, payload, authorize(token));
