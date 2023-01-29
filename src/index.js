54d70c32476afbe7ddfb30fb9265a58adabc61df
Krystian <krystianpiatkowski92@gmail.com>
2023-01-29 18:14:30 +0100

    Deleted redundand file (user-data.js)

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { LayoutProvider } from "./contexts/layout";
-import { UserDataProvider } from "./contexts/user-data";
 import { AuthContextProvider } from "./contexts/auth";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -11,9 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthContextProvider>
     <LayoutProvider>
-      <UserDataProvider>
-        <App />
-      </UserDataProvider>
+      <App />
     </LayoutProvider>
   </AuthContextProvider>
 );
