47cbf9c9a91f1de4e89a0b6386f09483089b3fb6
Krystian <krystianpiatkowski92@gmail.com>
2023-02-02 11:54:26 +0100

    Updated default colorTheme

--- src/utils/colorTheme.js
+++ src/utils/colorTheme.js
@@ -1,7 +1,7 @@
 // Generate an object with default colorTheme properties
 export const defaultColorTheme = () => [
-  { label: "Tło", property: "--bg-color", value: "#ffffff" },
-  { label: "Akcent", property: "--accent-color", value: "#705a89" },
+  { label: "Tło", property: "--bg-color", value: "#fffefa" },
+  { label: "Akcent", property: "--accent-color", value: "#a96060" },
   { label: "Tekst", property: "--font-color", value: "#333333" },
   { label: "Tag1", property: "--tag-0-color", value: "#ffe047" },
   { label: "Tag2", property: "--tag-1-color", value: "#7ab4ff" },
